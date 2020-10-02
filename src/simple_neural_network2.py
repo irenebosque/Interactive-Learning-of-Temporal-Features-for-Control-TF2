bd9c0dbb0d96e1fb4047cf0b6afe67998c61b91b
irenebosque <ireneboske@gmail.com>
2020-10-02 21:13:02 +0200

    simple conv working

--- src/simple_neural_network2.py
+++ src/simple_neural_network2.py
@@ -1,28 +1,34 @@
 import tensorflow as tf
 import numpy
 
-# input_encoder = numpy.zeros((64,64), dtype=float)
+input_encoder = tf.constant(1, dtype=tf.float32, shape=(1, 64, 64, 1))
+
+
+
 class NeuralNetwork(tf.keras.Model):
     def __init__(self, latent_dim):
         super(NeuralNetwork, self).__init__()
         self.latent_dim = latent_dim
-        self.Densa = tf.keras.layers.Dense(self.latent_dim, activation='relu')
-        '''
+        #self.Densa = tf.keras.layers.Dense(self.latent_dim, activation='relu')
+
         # Autoencoder encoder
-        self.conv1 = tf.keras.layers.Conv2D(16, [3, 3], stride=2, padding='same')
+        self.conv1 = tf.keras.layers.Conv2D(16, [3, 3], strides=2, padding='same')
+        '''
         # Autoencoder decoder
         self.deconv1 = tf.keras.layers.Conv2DTranspose(8, [3, 3], stride=2, padding='same')
         '''
 
 
     def call(self, x):
-        '''
+
         x = self.conv1(x)
+        '''
         x = self.deconv1(x)
         '''
-        x = self.Densa(x)
+        #x = self.Densa(x)
         return x
 
 
-neural_network = NeuralNetwork(latent_dim = 64)
-print(neural_network)
+neural_network = NeuralNetwork(123)#el 64 ahora esta de adorno
+neural_network(input_encoder)
+print(neural_network.summary())
