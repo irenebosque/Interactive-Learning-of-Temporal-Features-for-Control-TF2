5663dc7f463992608a4f92eb3d7d3674e80ca103
irenebosque <ireneboske@gmail.com>
2020-10-04 09:56:43 +0200

    - tf imported - ae_model_output eval changed - Train transition model, neural_network.sess.run(neural_network.train_transition_model, change to eager execution - state_representation = neural_network.sess.run change to eager execution

--- src/transition_model2.py
+++ src/transition_model2.py
@@ -2,6 +2,7 @@ import numpy as np
 from tools.functions import observation_to_gray, FastImagePlot
 from buffer import Buffer
 import cv2
+import tensorflow as tf
 
 """
 Transition model.
@@ -32,9 +33,11 @@ class TransitionModel:
         self.occlude_observation = occlude_observation
 
         # Buffers
-        self.last_actions = Buffer(min_size=self.training_sequence_length + 1, max_size=self.training_sequence_length + 1)
+        self.last_actions = Buffer(min_size=self.training_sequence_length + 1,
+                                   max_size=self.training_sequence_length + 1)
         self.last_actions.add(np.zeros([1, self.dim_a]))
-        self.last_states = Buffer(min_size=self.training_sequence_length + 1, max_size=self.training_sequence_length + 1)
+        self.last_states = Buffer(min_size=self.training_sequence_length + 1,
+                                  max_size=self.training_sequence_length + 1)
         self.last_states.add(np.zeros([1, self.image_width, self.image_width, 1]))
         self.transition_model_buffer_sampling_rate = buffer_sampling_rate
         self.transition_model_sampling_size = buffer_sampling_size
@@ -49,7 +52,8 @@ class TransitionModel:
 
     def _preprocess_observation(self, observation):
         if self.occlude_observation:
-            observation[48:, :, :] = np.zeros([48, 96, 3]) + 127  # TODO: occlusion should be a function of the input size
+            observation[48:, :, :] = np.zeros(
+                [48, 96, 3]) + 127  # TODO: occlusion should be a function of the input size
 
         if self.crop_observation:
             observation = observation[:, 80:-80]  # TODO: these numbers should not be hard coded
@@ -65,9 +69,8 @@ class TransitionModel:
         if self.t_counter % 4 == 0 and self.show_observation:
             self.state_plot.refresh(self.processed_observation)
 
-        if (self.t_counter+2) % 4 == 0 and self.show_ae_output:
-            ae_model_output = neural_network.transition_model_output.eval(session=neural_network.sess,
-                                                                          feed_dict={'transition_model/transition_model_input:0': self.network_input[-1]})
+        if (self.t_counter + 2) % 4 == 0 and self.show_ae_output:
+            ae_model_output = neural_network(self.network_input[-1])
 
             self.ae_output_plot.refresh(ae_model_output)
 
@@ -90,15 +93,18 @@ class TransitionModel:
                         print('Database too small for training!')
                         return
 
-                    selected_episode = round(np.random.uniform(-0.49, episodes_num - 1))  # select and episode from the database randomly
+                    selected_episode = round(
+                        np.random.uniform(-0.49, episodes_num - 1))  # select and episode from the database randomly
                     episode_trajectory_length = len(database[selected_episode])
 
                     if episode_trajectory_length > self.training_sequence_length + 2:
                         break
 
-                sequence_start = round(np.random.uniform(0, episode_trajectory_length - self.training_sequence_length - 1))
+                sequence_start = round(
+                    np.random.uniform(0, episode_trajectory_length - self.training_sequence_length - 1))
 
-                sequence = database[selected_episode][sequence_start:sequence_start + self.training_sequence_length + 1]  # get samples from database
+                sequence = database[selected_episode][
+                           sequence_start:sequence_start + self.training_sequence_length + 1]  # get samples from database
 
                 observation_seq = []
                 action_seq = []
@@ -112,38 +118,75 @@ class TransitionModel:
                 actions.append(action_seq[:-1])
                 predictions.append(observation_seq[-1])
 
-            #observations = np.array(observations)  TODO: these are required for LSTM training
-            #actions = np.array(actions)
+            # observations = np.array(observations)  TODO: these are required for LSTM training
+            # actions = np.array(actions)
             predictions = np.array(predictions)
 
             # Train transition model
-            neural_network.sess.run(neural_network.train_transition_model,
-                                    feed_dict={'transition_model/transition_model_input:0': np.reshape(predictions, [self.transition_model_sampling_size, self.image_width, self.image_width, 1]),
-                                               'transition_model/transition_model_label:0': np.reshape(predictions, [self.transition_model_sampling_size, self.image_width, self.image_width, 1])})
+
+            input_to_encoder = np.reshape(predictions,
+                                          [self.transition_model_sampling_size, self.image_width, self.image_width, 1])
+
+            output_from_decoder = neural_network(input_to_encoder)  # --> model(input)
+
+            '''
+            # Printing out some values just to see how they are
+            difference_result = input_to_encoder - output_from_decoder
+            #print(difference_result)
+            print(difference_result.dtype)
+            print(tf.shape(difference_result))
+            loss_function = tf.keras.losses.MSE(input_to_encoder, output_from_decoder)
+            print('LOSS:')
+            #print(loss_function)
+            '''
+            neural_network.compile(optimizer='adam', loss=tf.keras.losses.MeanSquaredError())
+
+            '''
+            My guess is that when using the code above you do not need to specify the loss function because it does it
+            "automatically". Meaning that it knows that 'output_from_decoder' is the prediction and 'input_to_encoder 
+            is the target.
+
+            # The explanation for the arguments of the fit function was taken from:
+            https://www.tensorflow.org/tutorials/generative/autoencoder
+            "Train the model using x_train as both the input and the target. The encoder will learn to compress the 
+            dataset from 784 dimensions to the latent space, and the decoder will learn to reconstruct the original images." 
+
+            '''
+
+            neural_network.fit(input_to_encoder, input_to_encoder)
 
     def train(self, neural_network, t, done, database):
         # Transition model training
-        if (t % self.transition_model_buffer_sampling_rate == 0 and t != 0) or (self.train_end_episode and done):  # Sim pendulum: 200; mountain car: done TODO: check if use done
+        if (t % self.transition_model_buffer_sampling_rate == 0 and t != 0) or (
+                self.train_end_episode and done):  # Sim pendulum: 200; mountain car: done TODO: check if use done
             self._train_model_from_database(neural_network, database)
 
     def get_state_representation(self, neural_network, observation):
         self._preprocess_observation(np.array(observation))
 
-        state_representation = neural_network.sess.run(neural_network.state_representation,
-                                                       feed_dict={'transition_model/transition_model_input:0': self.network_input[-1]})
+        state_representation = neural_network(self.network_input[-1])
 
         self._refresh_image_plots(neural_network)  # refresh image plots
         self.t_counter += 1
         return state_representation
 
-    def get_state_representation_batch(self, neural_network, observation_sequence_batch, action_sequence_batch, current_observation):
+    def get_state_representation_batch(self, neural_network, observation_sequence_batch, action_sequence_batch,
+                                       current_observation):
         batch_size = len(observation_sequence_batch)
 
         lstm_hidden_state_batch = neural_network.sess.run(neural_network.lstm_hidden_state,
-                                                          feed_dict={'transition_model/transition_model_input:0': np.reshape(observation_sequence_batch, [batch_size, self.image_width, self.image_width, 1])})
+                                                          feed_dict={
+                                                              'transition_model/transition_model_input:0': np.reshape(
+                                                                  observation_sequence_batch,
+                                                                  [batch_batch_size, self.image_width, self.image_width,
+                                                                   1])})
 
         state_representation_batch = neural_network.sess.run(neural_network.state_representation,
-                                                             feed_dict={'transition_model/transition_model_input:0': np.reshape(current_observation, [batch_size, self.image_width, self.image_width, 1])})
+                                                             feed_dict={
+                                                                 'transition_model/transition_model_input:0': np.reshape(
+                                                                     current_observation,
+                                                                     [batch_size, self.image_width, self.image_width,
+                                                                      1])})
 
         return state_representation_batch
 
@@ -151,7 +194,8 @@ class TransitionModel:
         action = np.reshape(action, [1, self.dim_a])
 
         self.lstm_hidden_state = neural_network.sess.run(neural_network.lstm_hidden_state,
-                                                         feed_dict={'transition_model/transition_model_input:0': self.network_input[-1]})
+                                                         feed_dict={'transition_model/transition_model_input:0':
+                                                                        self.network_input[-1]})
         self.last_actions.add(action)
 
     def last_step(self, action_label):
@@ -165,7 +209,9 @@ class TransitionModel:
 
     def new_episode(self):
         self.lstm_hidden_state = np.zeros([1, 2 * self.lstm_h_size])
-        self.last_states = Buffer(min_size=self.training_sequence_length + 1, max_size=self.training_sequence_length + 1)
-        self.last_actions = Buffer(min_size=self.training_sequence_length + 1, max_size=self.training_sequence_length + 1)
+        self.last_states = Buffer(min_size=self.training_sequence_length + 1,
+                                  max_size=self.training_sequence_length + 1)
+        self.last_actions = Buffer(min_size=self.training_sequence_length + 1,
+                                   max_size=self.training_sequence_length + 1)
         self.last_actions.add(np.zeros([1, self.dim_a]))
         self.last_states.add(np.zeros([1, self.image_width, self.image_width, 1]))
